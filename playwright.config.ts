601e9ccab7247d24d84fe40eef76d11300193774
Vivian <vivian.hoang@724softwares.com>
2026-05-22 15:02:02 +0700

    Remove storage state from Playwright configuration and add a blank line in practice test file

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,12 +38,7 @@ export default defineConfig({
       use: {
 
         ...devices['Desktop Chrome'],
-
-        storageState:
-          'playwright/.auth/user.json',
-
       },
-
       dependencies: ['setup'],
     },
 
--- tests/practice.spec.ts
+++ tests/practice.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 
+
 test('Search playing cards', async ({ page }) => {
     await page.goto('https://www.qpmarketnetwork.com/')
 
